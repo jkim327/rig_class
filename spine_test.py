ee10b92c8389b54409fad56393fcffdd64f896bd
jkim327 <61069690+jkim327@users.noreply.github.com>
2020-04-29 01:02:37 +0900

    UI_test.

---
+++ spine_test.py
@@ -0,0 +1,29 @@
+"""
+1. call guide skeleton
+2. receive guide skeleton information
+3. create final skeleton
+4. orient skeleton
+
+Fk
+5. create controls
+6. position controls
+7. constraint controls to skeleton
+9. constraint controls
+
+IK
+5. create ik spline handle
+6. 
+"""
+
+import maya.cmds as cmds
+
+class sample_spine(object):
+    def __init__(self):
+        
+    def create_sample(self):
+        num = 
+
+class FK_rig(object):
+    
+    def __init__(self):
+        pass
